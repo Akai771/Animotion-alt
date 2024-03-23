fec110a5f7c68d0881934ad23019a39d26dd79bb
Akai <rishabh.hs@somaiya.edu>
2024-03-24 01:49:13 +0530

    feat: add new images to carousel

--- Animotion/src/Components/Home/Carousel/CarouselImage.jsx
+++ Animotion/src/Components/Home/Carousel/CarouselImage.jsx
@@ -1,15 +1,15 @@
 const data = [
     {
       Logo: "https://freepngimg.com/thumb/one_piece/22924-7-one-piece-logo-image.png",
-      imageUrl: "https://a-static.besthdwallpaper.com/one-piece-monkey-d-luffy-gear-5-sun-god-wallpaper-2560x1080-96926_14.jpg", 
+      imageUrl: "https://i.postimg.cc/d3vq03NY/one-piece-monkey-d-luffy-gear-5-sun-god-wallpaper-2560x1080-14.jpg", 
       title:"One Piece",
       season: '20',
       description:"Monkey. D. Luffy refuses to let anyone or anything stand in the way of his quest to become the king of all pirates. With a course charted for the treacherous waters of the Grand Line and beyond, this is one captain who'll never give up until he's claimed the greatest treasure on Earth: the Legendary One Piece!",
       url : "one-piece-100",
      },
      {
-      Logo: "https://i.postimg.cc/ZYf5jJFp/solo-Leveling-2.png",
-      imageUrl: "https://images6.alphacoders.com/129/1293436.jpg",
+      Logo: "https://i.postimg.cc/sXGc2DQw/solo-Leveling-2.png",
+      imageUrl: "https://i.postimg.cc/SsgZntDx/solo-Leveling.jpg",
       title:"Solo Leveling",
       season: '1',
       description:"Ten years ago, 'the Gate' appeared and connected the real world with the realm of magic and monsters. To combat these vile beasts, ordinary people received superhuman powers and became known as 'Hunters...",
@@ -17,7 +17,7 @@ const data = [
      },
      {
       Logo: "https://1000logos.net/wp-content/uploads/2022/06/Demon-Slayer-Logo-500x281.png",
-      imageUrl: "https://4kwallpapers.com/images/wallpapers/demon-slayer-1920x1080-10716.jpg", 
+      imageUrl: "https://i.postimg.cc/PqG0HcRG/demon-slayer.jpg", 
       title:"Demon Slayer",
       season: '3',
       description:"It is the Taisho Period in Japan. Tanjiro, a kindhearted boy who sells charcoal for a living, finds his family slaughtered by a demon. To make matters worse, his younger sister Nezuko, the sole survivor, has been transformed into a demon herself....",
@@ -25,7 +25,7 @@ const data = [
      },
      {
       Logo: "https://upload.wikimedia.org/wikipedia/commons/4/49/Jujutsu_Kaisen_logo.png",
-      imageUrl: "https://staticg.sportskeeda.com/editor/2023/05/3780e-16830225202726-1920.jpg", 
+      imageUrl: "https://i.postimg.cc/qBXKx3T2/jujutsu-Kaisen.avif", 
       title:"Jujustu Kaisen",
       season: '2',
       description:"Idly indulging in baseless paranormal activities with the Occult Club, high schooler Yuuji Itadori spends his days at either the clubroom or the hospital, where he visits his bedridden grandfather. However, this leisurely lifestyle soon takes a turn for the strange when he unknowingly encounters a cursed item....",
@@ -33,15 +33,15 @@ const data = [
      },
      {
       Logo: "https://vectorseek.com/wp-content/uploads/2023/09/Haikyuu-Logo-Vector.svg-.png",
-      imageUrl: "https://wallpapercave.com/wp/wp5457390.jpg", 
+      imageUrl: "https://i.postimg.cc/zvPPcPgc/haikyuu.webp", 
       title:"Haikyuu",
       season: '4',
       description:"After their triumphant victory over Shiratorizawa Academy, the Karasuno High School volleyball team has earned their long-awaited ticket to nationals. As preparations begin, genius setter Tobio Kageyama is invited to the All-Japan Youth Training Camp to play alongside fellow nationally recognized players. Meanwhile, Kei Tsukishima is invited to a special rookie training camp for first-years within the Miyagi Prefecture...",
       url : "haikyuu-to-the-top-part-12-122",
      },
      {
-      Logo: "https://i.postimg.cc/L5msrHYK/bleach-logo.png",
-      imageUrl: "https://i.postimg.cc/k4zccpBb/bleach-thousand-year-blood-war-hd-wallpaper-uhdpaper-com-812-1-i.jpg", 
+      Logo: "https://i.postimg.cc/GpcJvxB8/bleach-logo.png",
+      imageUrl: "https://i.postimg.cc/C1x7psST/bleach-thousand-year-blood-war-hd-wallpaper-uhdpaper-com-812-1-i.jpg", 
       title:"Bleach: TYBW",
       season: '5',
       description:"After a brutal surprise attack by the forces of Quincy King Yhwach, the resident Reapers of the Soul Society lick their wounds and mourn their losses. Many of the surviving Soul Reaper captains train to battle without their Bankai, the ultimate technique wielded ...",
