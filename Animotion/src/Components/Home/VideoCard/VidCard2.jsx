fd1d0910e2124433db49f83ccce9ac26c3f6d75c
Akai <rishabh.hs@somaiya.edu>
2025-01-18 20:08:32 +0530

    feat: Added Qtip for each card

--- Animotion/src/Components/Home/VideoCard/VidCard2.jsx
+++ Animotion/src/Components/Home/VideoCard/VidCard2.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./VidCard3.css";
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import axios from "axios";
+import Tooltip from "@mui/material/Tooltip";
+import ClosedCaptionOffIcon from '@mui/icons-material/ClosedCaptionOff';
+import MicNoneIcon from '@mui/icons-material/MicNone';
+import StarIcon from '@mui/icons-material/Star';
+
+
 
 const VidCard2 = ({id, title, coverImage, type, duration}) => {
+  const [qtipData, setQtipData] = useState([]);
+  useEffect(()=>{
+      axios.get(`${import.meta.env.VITE_API}/api/v2/hianime/qtip/${id}`)
+      .then((res) => {
+        setQtipData(res.data.data.anime)
+      })
+      .catch((err) => {
+        console.log("Error Fetching Qtip Data: ",err);
+      })
+  },[])
+  
+  const episodeSub = qtipData.episodes?.sub || "N/A";
+  const episodeDub = qtipData.episodes?.dub || "N/A";
+  const genres = qtipData.genres || [];
+
   return (
     <>
       <div class="trialCard">
+      <Tooltip title={
+              <div className="Tooltip">
+                <span className="TooltipTitle">{qtipData.name}</span>
+                <div className="TooltipQuickInfo">
+                  <div className="TooltipQuickInfoBoxGold"><StarIcon style={{fontSize:"1.2rem", marginRight:".2rem"}} /> {qtipData.malscore}</div>
+                  <div className="TooltipQuickInfoBox">{qtipData.type}</div>
+                  <div className="TooltipQuickInfoBox"><ClosedCaptionOffIcon style={{fontSize:"1.2rem", marginRight:".2rem"}}/> {episodeSub}</div>
+                  <div className="TooltipQuickInfoBox"><MicNoneIcon style={{fontSize:"1.2rem", marginRight:".2rem"}} /> {episodeDub}</div>
+                </div>
+                <div className="TooltipContBox">
+                  <span className="TooltipContTitle">Jname:  <span className="TooltipCont">{qtipData.jname?qtipData.jname:"N/A"} </span></span>
+                  <span className="TooltipContTitle">Genres: <span className="TooltipCont">{genres.join(", ")} </span></span>
+                  <span className="TooltipContTitle">Aired: <span className="TooltipCont">{qtipData.aired} </span></span>
+                  <span className="TooltipContTitle">Status: <span className="TooltipCont">{qtipData.status} </span></span>
+                  <span className="TooltipContTitle">Desc: <span className="TooltipCont">{qtipData.description? qtipData.description: "N/A"} </span></span>
+                </div>
+              </div>
+          } placement="right" arrow disableInteractive>
           <div className="trialCardImage">
             <Link exact to={`/details/${id}`}>
                 <img className="trialImage" src={coverImage} alt={title} />
@@ -25,6 +65,7 @@ const VidCard2 = ({id, title, coverImage, type, duration}) => {
                   <span class="trialTitle">{title}</span>
               </div>
           </div>
+      </Tooltip>
       </div>
     </>
   );
