7329b580895ee2194ad2b90a9ee064a9ab75b066
Akai <rishabh.hs@somaiya.edu>
2025-02-06 21:25:22 +0530

    fix: fixed video player volume issue

--- Animotion/src/components/video-player.tsx
+++ Animotion/src/components/video-player.tsx
@@ -25,19 +25,6 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ serverLink, trackSrc =[], mal
   const trackSource = trackSrc.filter((track) => track.label === "English");
   const track = trackSource.length > 0 ? trackSource[0].file : "";
 
-//   const volume = useMediaStore((store) => store.volume);
-
-//   useEffect(() => {
-//     if (volume !== 50) {
-//       setTimeout(() => {
-//         const mediaElement = document.querySelector("media-player") as HTMLMediaElement | null;
-//         if (mediaElement) {
-//           mediaElement.volume = 0.5;
-//         }
-//       }, 0);
-//     }
-//   }, [volume]);
-
   // Placeholder image
   const placeholderImage = "https://placehold.jp/000000/ffffff/1920x1080.png?text=Loading...&css=%7B%22font-weight%22%3A%22%20700%22%7D";
 
@@ -51,7 +38,7 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ serverLink, trackSrc =[], mal
           autoplay
           crossorigin="anonymous"
           playsInline
-        //   volume={0.5}
+          volume={0.5}
         >
           <MediaProvider>
             <PlayButton />
